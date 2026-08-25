4e5dbfa28ff294b43bad28d5a19c7f37fa648cfd
awchjimmy <awchjimmy@gmail.com>
2026-08-25 16:34:41 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,14 +3,21 @@ import sys
 
 def main():
     while True:
-        cmd = input('$ ')
-        if cmd != "exit":
-            handle_command(cmd)
+        user_input = input('$ ')
+        if user_input != "exit":
+            handle_command(user_input)
         else:
             break
     
-def handle_command(cmd):
-    print(f'{cmd}: command not found')
+def handle_command(user_input):
+    parts = user_input.split(' ')
+    cmd = parts[0]
+
+    if cmd == "echo":
+        line = ' '.join(parts[1:])
+        print(line)
+    else:
+        print(f'{user_input}: command not found')
 
 
 if __name__ == "__main__":
