3fef639d26cd4f169c3df1528d89ad96bb17399e
awchjimmy <awchjimmy@gmail.com>
2026-08-25 16:38:56 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -16,8 +16,26 @@ def handle_command(user_input):
     if cmd == "echo":
         line = ' '.join(parts[1:])
         print(line)
+    elif cmd == "type":
+        line = ' '.join(parts[1:])
+        output = execute_type_command(line)
+        print(output)
+
     else:
         print(f'{user_input}: command not found')
+
+
+
+def execute_type_command(line):
+    if line == "exit":
+        output = "exit is a shell builtin"
+    elif line == "echo":
+        output = "echo is a shell builtin"
+    elif line == "type":
+        output = "type is a shell builtin"
+    else:
+        output = f"{line}: not found"
+    return output
 
 
 if __name__ == "__main__":
